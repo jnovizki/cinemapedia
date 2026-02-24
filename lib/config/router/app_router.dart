5d0dc2f5ae22016f1f9b7e191933b1078f443054
JNovizki <jnovizki@yahoo.com>
2026-02-23 21:41:19 -0300

    Se crean los archivos fundamentales de la arquitectura.

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,7 @@
+import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [GoRoute(path: '/', name: HomeScreen.name, builder: (context, stater) => const HomeScreen())],
+);
