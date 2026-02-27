8cfd989554cddeba785046690362919d63429f80
JNovizki <jnovizki@yahoo.com>
2026-02-27 12:28:49 -0300

    Environment variables configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MainApp());
 }
 
@@ -11,8 +13,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: appRouter, debugShowCheckedModeBanner: false,
-      theme: AppTheme().getTheme());
+    return MaterialApp.router(routerConfig: appRouter, debugShowCheckedModeBanner: false, theme: AppTheme().getTheme());
   }
 }
